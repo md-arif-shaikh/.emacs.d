fcfdaf3a4e46ef8d63c0cbe49a8936ea9fdc31d1
md-arif-shaikh <arifshaikh.astro@gmail.com>
2021-04-19 11:35:40 +0530

    script to build emacs from source, python-lsp

---
+++ build_emacs.sh
@@ -0,0 +1,13 @@
+git clone --single-branch --branch emacs-27 --depth 1 https://github.com/emacs-mirror/emacs.git
+cd emacs/
+sudo apt install -y autoconf make gcc texinfo libxpm-dev \
+     libjpeg-dev libgif-dev libtiff-dev libpng-dev libgnutls28-dev \
+     libncurses5-dev libjansson-dev libharfbuzz-dev libx11-dev libgtk-3-dev
+./autogen.sh
+./configure --with-json --with-modules --with-harfbuzz --with-compress-install \
+            --with-threads --with-included-regex --with-zlib --with-cairo --without-rsvg\
+            --without-sound --without-imagemagick  --without-toolkit-scroll-bars \
+            --without-gpm --without-dbus --without-makeinfo --without-pop \
+            --without-mailutils --without-gsettings
+make -j$(nproc)
+sudo make install-strip
