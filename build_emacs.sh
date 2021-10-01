3ac099d1f4ae85698be2afc5e4a7cd6e725efc55
md-arif-shaikh <arifshaikh.astro@gmail.com>
2021-10-01 16:32:51 +0530

    build script modified

--- build_emacs.sh
+++ build_emacs.sh
@@ -1,10 +1,12 @@
 if test -d "$HOME/emacs/"; then
-    echo "...emacs is already cloned in the home directory"
+    echo "...emacs is already cloned in the home directory. pulling changes."
+    cd "$HOME/emacs/"
+    git pull --rebase
 else
     # git clone --single-branch --branch emacs-27 --depth 1 https://github.com/emacs-mirror/emacs.git "$HOME/emacs"
     git clone git://git.savannah.gnu.org/emacs.git "$HOME/emacs"
+    cd "$HOME/emacs/"
 fi
-cd "$HOME/emacs/"
 
 if test -f "/etc/debian_version"; then
     echo "...system is debian. Going ahead to install the dependencies."
