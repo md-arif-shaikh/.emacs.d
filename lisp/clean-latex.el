94acae25de4cb2193fce6bc09e2d65df162b3ade
Arif Shaikh <arif.shaikh@icts.res.in>
2022-02-18 23:17:44 +0530

    add more extension

--- lisp/clean-latex.el
+++ lisp/clean-latex.el
@@ -1,4 +1,4 @@
-;; clean latex auxiliary files for a given directory
+;; clean up LaTeX auxiliary files in a given directory
 (defun clean-latex (directory)
   "Clean all the auxiliary files generated due to LaTeX compilation
 in a given DIRECTORY."
@@ -6,7 +6,7 @@ in a given DIRECTORY."
   (let ((files '())
 	(num-of-files 0)
 	(cleanp))
-    (dolist (extension '("out" "aux" "log" "gz" "bbl"))
+    (dolist (extension '("out" "aux" "log" "gz" "bbl" "pytxcode" "toc" "bcf"))
       (push (file-expand-wildcards (format "%s*.%s" directory extension)) files))
     (setq files (flatten-list files))
     (setq num-of-files (length files))
