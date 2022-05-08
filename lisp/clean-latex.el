6b02029654a532f13ba8d349be19e97e676c5512
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-05-08 17:36:44 +0530

    remove buffer name from tab-bar

--- lisp/clean-latex.el
+++ lisp/clean-latex.el
@@ -1,17 +1,18 @@
 ;; clean up LaTeX auxiliary files in a given directory
-(defun clean-latex (directory)
+(defun clean-latex (directory &optional cleanp)
   "Clean all the auxiliary files generated due to LaTeX compilation
 in a given DIRECTORY."
   (interactive (list (read-file-name "Enter directory: ")))
   (let ((files '())
-	(num-of-files 0)
-	(cleanp))
-    (dolist (extension '("out" "aux" "log" "gz" "bbl" "pytxcode" "toc" "bcf"))
+	(num-of-files 0))
+    (dolist (extension '("out" "aux" "log" "gz" "bbl" "pytxcode" "toc" "bcf" "sty"))
       (push (file-expand-wildcards (format "%s*.%s" directory extension)) files))
     (setq files (flatten-list files))
     (setq num-of-files (length files))
-    (setq cleanp (string-equal (completing-read (format "Clean %s files: " num-of-files) '("yes" "no")) "yes"))
+    (when (equal cleanp nil)
+	(setq cleanp (string-equal (completing-read (format "Clean %s files: " num-of-files) '("yes" "no")) "yes")))
     (when cleanp
       (dolist (file files)
 	(delete-file file))
-      (message (format "cleaned %s files." num-of-files)))))
+      (message (format "cleaned %s files." num-of-files)))
+    num-of-files))
