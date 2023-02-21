cbbaa1ac159fed13a3c1739fa08c12b9e4eec436
md-arif-shaikh <arifshaikh.astro@gmail.com>
2023-02-21 22:41:14 +0900

    change to dark one

--- emacs.el
+++ emacs.el
@@ -47,7 +47,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
 	doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-gruvbox t)
+  (load-theme 'doom-one t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
@@ -85,7 +85,7 @@
 (set-face-attribute 'default nil
 		    :font "JetBrains Mono"
 		    :weight 'light
-		    :height (cond ((string-equal system-type "gnu/linux") 120)
+		    :height (cond ((string-equal system-type "gnu/linux") 110)
 				  ((string-equal system-type "darwin") 130)))
 
 ;;(set-face-font 'default "fontset-default")
@@ -720,3 +720,5 @@
 
 (use-package vterm
   :straight t)
+
+(setq-default fill-column 79)
